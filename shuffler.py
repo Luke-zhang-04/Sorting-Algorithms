5a5c78f0a72d791d8a749e4ce13bac42e1d37708
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-12-28 19:21:29 -0500

    modified shuffler

--- shuffler.py
+++ shuffler.py
@@ -1,6 +1,7 @@
 from random import shuffle
 
-def random_sequence(minimum, maximum): #returns a shuffled array
-    array = [i for i in range(minimum, maximum)]
+def random_sequence(*args): #returns a shuffled array
+    if len(args) == 2: array = [i for i in range(args[0], args[1])]
+    elif len(args) == 3: array = [i for i in range(args[0], args[1], args[2])]
     shuffle(array)
     return array
