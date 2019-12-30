6ab215b49c81e1fd48e2b372cb68e53488c5b42f
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-12-29 19:54:43 -0500

    added animator

---
+++ animator.py
@@ -0,0 +1,24 @@
+from time import sleep
+from random import randint
+
+def draw_graph(array):
+    import __main__
+    screen = __main__.screen
+    screen.delete("all")
+    
+    yIncrement = (int(screen['height'])*0.975) / max(array)
+    xIncrement = int(screen['width']) / len(array)
+    colour = "black" if screen['background'] == "white" else "white"
+    outline = "white" if colour == "black" else "black"
+
+    for i in range(len(array)):
+        screen.create_rectangle(i*xIncrement, int(screen['height']), (i+1)*xIncrement, int(screen['height']) - array[i]*yIncrement, fill = colour, outline = outline, width = 0.1)
+    
+    screen.update()
+
+    if len(array) <= 100:
+        sleep(0.05)
+    else:
+        __main__.counter += 1
+        if __main__.counter % 10 == 0:
+            sleep(0.001)
---
+++ bubble-Sort/bubble-sort-animated.py
@@ -0,0 +1,33 @@
+import os, sys #import animator from parent directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+sys.path.insert(0, parent_dir_path)
+
+from animator import draw_graph
+from shuffler import random_sequence
+counter = 0
+
+from tkinter import Tk, Canvas
+myInterface = Tk()
+screen = Canvas(myInterface, width=1000, height=800, background = "black")
+screen.pack()
+
+def bubble_sort(array): #BUBBLE SORT
+    for amt in range(len(array)):
+        swaps = 0
+        for i in range(len(array)-1-amt): #iterate through the array
+            if array[i] > array[i + 1]: #swap if needed
+                array[i], array[i+1] = array[i+1], array[i]
+                swaps += 1
+                draw_graph(array)
+        if swaps == 0: break #array is in order
+
+    return array
+
+shuffled_array = random_sequence(0, 50)
+
+draw_graph(shuffled_array)
+
+bubble_sort(shuffled_array)
+
+screen.mainloop()
