53a403a40fd44534f4ca7849b96e9e5c0ac569e5
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-12-29 20:55:50 -0500

    fixed cocktail shaker sort py

--- cocktail-shaker-sort/cocktail-shaker-sort.py
+++ cocktail-shaker-sort/cocktail-shaker-sort.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 
     print("COCKTAIL SHAKER SORT")
 
-    shuffled_array = random_sequence(0, 10)
+    shuffled_array = random_sequence(0, 1000)
 
     print(shuffled_array, "\n")
 
