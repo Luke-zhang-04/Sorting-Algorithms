97e74436bc21ad82ba74b86a3777f525c23b0ce0
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-06-12 11:04:38 -0400

    reformat code with psf/black
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- bogoSort/bogosort.py
+++ bogoSort/bogosort.py
@@ -1,14 +1,16 @@
-#if shuffled_array = random_sequence(0, x <= 5), the algorithm should work fine. Otherwise, a recursion error is likely
+# if shuffled_array = random_sequence(0, x <= 5), the algorithm should work fine. Otherwise, a recursion error is likely
 
 from random import shuffle
+
 
 def issorted(array):
     """Checks if an array is sorted
     Param array is an array of numbers
     Returns true if sorted, false if not
     """
-    for i in range(len(array)-1):
-        if array[i+1] < array[i]: return False
+    for i in range(len(array) - 1):
+        if array[i + 1] < array[i]:
+            return False
 
     return True
 
@@ -16,13 +18,17 @@ def issorted(array):
 def bogoSort(array):
     """Main bogosort function"""
     shuffle(array)
-    return array if issorted(array) else bogoSort(array) # Check if the array is sorted, otherwise recurse
+    return (
+        array if issorted(array) else bogoSort(array)
+    )  # Check if the array is sorted, otherwise recurse
+
 
 if __name__ == "__main__":
     try:
         from shuffler import random_sequence
     except ModuleNotFoundError:
-        import os, sys #import shuffler from parent directory
+        import os, sys  # import shuffler from parent directory
+
         dir_path = os.path.dirname(os.path.realpath(__file__))
         parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
         sys.path.insert(0, parent_dir_path)
