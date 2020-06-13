ab4b5608d5917e49d78c7eca88611652c3224b07
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-06-12 23:23:07 -0400

    fix(countingSort): fix JS/TS style and syntax errors
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- countingSort/index.d.ts
+++ countingSort/index.d.ts
@@ -1,1 +1,7 @@
-export {};
+/**
+ * Main countingsort function
+ * @param {Array.<number>} array - array to sort
+ * @returns {Array.<number | null>} sorted array; does not sort in-place
+ */
+declare const countingSort: (array: number[]) => (null | number)[];
+export default countingSort;
