2e4aa999257ed71294ab312944089734441079bc
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-08-17 10:39:44 -0400

    remove generated javascript
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- utils/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Generates an array of random numbers
- * @param {number} minimum - num number to start with
- * @param {number} maximum - max number to start with
- * @returns {Array.<number>} array of random numbers
- */
-declare const randomSequence: (minimum: number, maximum: number) => number[];
-export default randomSequence;
